8f0348c882bf42d1f8e2de36e53723f327f776f8
teoma145 <matteodark145@yahoo.it>
2024-04-05 16:27:51 +0200

    first bonus

--- src/org/wishlist/listaRegali.java
+++ src/org/wishlist/listaRegali.java
@@ -5,12 +5,17 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class listaRegali {
+    public static final String FILE_PATH = "data.txt";
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         List<String> wishList = new ArrayList<>();
-
+        loadWishListFromFile(wishList);
         boolean exit = false;
         while (!exit) {
             System.out.print("Vuoi inserire un regalo? (y/n): ");
@@ -30,6 +35,7 @@ public class listaRegali {
                     break;
             }
         }
+        saveWishListToFile(wishList);
         if (wishList.isEmpty()) {
             System.out.println("La tua lista dei desideri è vuota.");
         } else {
@@ -43,5 +49,25 @@ public class listaRegali {
         }
 
         scan.close();
+    }
+    private static void loadWishListFromFile(List<String> wishList) {
+        File file = new File(FILE_PATH);
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                wishList.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("la lista è vuota");
+        }
+    }
+    private static void saveWishListToFile(List<String> wishList) {
+        try (FileWriter writer = new FileWriter(FILE_PATH)) {
+            for (String regalo : wishList) {
+                writer.write(regalo + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("impossibile salvare");
+        }
     }
 }
